14657189e83b3d97e96a6457cae12f837f6ebc1d
NusratxojaGanixojayev <ganixojayev91@gmail.com>
2022-02-03 21:20:41 +0500

    38 dars qiziq ekan

---
+++ src/main/java/hibernate_test/entity/Test1.java
@@ -0,0 +1,27 @@
+package hibernate_test.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Nusratxoja", "Ganixojayev",
+                    "IT", 350);
+            session.beginTransaction();
+            session.save(emp);
+            session.getTransaction().commit();
+        }
+        finally {
+            factory.close();//Sessiyani yopish shart
+        }
+
+    }
+}
